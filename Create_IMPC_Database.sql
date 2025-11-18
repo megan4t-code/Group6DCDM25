e2b1c90b90a1f264ed078394fe41e69683d17d9c
megan4t-code <megan4tucker@gmail.com>
2025-11-18 16:31:38 +0000

    Changed table names and string lengths to accomodate data

--- Create_IMPC_Database.sql
+++ Create_IMPC_Database.sql
@@ -4,45 +4,45 @@
 -- In the statements below, each table is created by defining its name, the column names, the data types they store, and their maximum allowed lengths.
 -- Constraints are also included to enforce rules on the data, in this case, the primary keys, preventing invalid or inconsistent values.
 
-create table impc_disease (
+create table Disease (
     disease_id varchar(20) not null,
     disease_name varchar(150),
-    omim_ids varchar(600),
-    constraint impc_disease_pk primary key (disease_id)
+    omim_id TEXT, #changed to TEXT from varchar
+    constraint disease_pk primary key (disease_id)
 );
 
-create table impc_parameter (
-impc_parameter_orig_id int not null,
-parameter_code varchar(20),
-parameter_name varchar(74) not null,
-description varchar (400),
-contraint impc_parameter_pk primary key (impc_parameter_orig_id)
+create table Parameter (
+    impc_parameter_orig_id int not null,
+    parameter_code varchar(20),
+    parameter_name varchar(100) not null, #100 characters to be changed to 74
+	description TEXT,
+	constraint parameter_pk primary key (impc_parameter_orig_id)
 );
 
-create table impc_parameter_group (
+create table Procedures (
+    procedure_id int auto_increment not null,
+    procedure_name varchar(50) not null,
+    description TEXT, #changed to TEXT from varchar
+    is_mandatory tinyint(1) not null default 0,
+    constraint procedures_pk primary key (procedure_id)
+);
+
+create table Analysis (
+    analysis_id varchar(25) not null,
+    mgi_accession_id varchar(25) not null,
+    gene_symbol varchar(25) not null,
+    mouse_strain varchar(5),
+    mouse_life_stage varchar(25),
+    impc_parameter_orig_id int,
+    pvalue double,
+    constraint analysis_pk primary key (analysis_id),
+    foreign key (impc_parameter_orig_id) references Parameter (impc_parameter_orig_id)
+);
+
+create table ParameterGroup (
     parameter_group_id int auto_increment not null,
     group_name varchar(100) not null,
     constraint impc_parameter_group_pk primary key (parameter_group_id)
-);
-
-create table impc_procedure (
-    procedure_id int auto_increment not null,
-    procedure_name varchar(50) not null,
-    description varchar(600),
-    is_mandatory tinyint(1) not null default 0,
-    constraint impc_procedure_pk primary key (procedure_id)
-);
-
-create table impc_analysis (
-    analysis_id varchar(15) not null,
-    mgi_accession_id varchar(11) not null,
-    gene_symbol varchar(13) not null,
-    mouse_strain varchar(5),
-    mouse_life_stage varchar(17),
-    impc_parameter_orig_id int not null,
-    pvalue double,
-    constraint impc_analysis_pk primary key (analysis_id),
-    foreign key (impc_parameter_orig_id) references impc_parameter (impc_parameter_orig_id)
 );
 
 -- [CREATING INDEXES]
@@ -52,15 +52,15 @@ create table impc_analysis (
 -- it's better to create them before the joining tables
 
 create index idx_disease_name 
-    on impc_disease (disease_name);
+    on Disease (disease_name);
 
 create index idx_parameter_name 
-    on impc_parameter (parameter_name);
+    on Parameter (parameter_name);
 
 create index idx_analysis_columns 
-    on impc_analysis (mgi_accession_id, impc_parameter_orig_id, gene_symbol);
+    on Analysis (mgi_accession_id, impc_parameter_orig_id, gene_symbol);
 
--- [CREATING JOINING TABLES] -- 
+-- [CREATING JOINING TABLES] -- #MEGAN HAS NOT TOUCHED THESE YET
 
 create table impc_analysis_disease (
     mgi_accession_id varchar(11) not null,
@@ -90,11 +90,12 @@ create table impc_procedure_parameter (
 -- So a value in one table must already exist in another table before it can be referenced.
 
 -- table dependency order:
--- impc_disease
--- impc_parameter
--- impc_parameter_group
--- impc_procedure
--- impc_analysis
+-- Disease
+-- Parameter
+-- ParameterGroup
+-- Procedure
+-- Analysis
 -- impc_analysis_disease
 -- impc_parameter_group_member
 -- impc_procedure_parameter
+
